886a07c77a6d33d32f2553079c2b4e2b8a171f20
Stanley Pan <imoment.developer@gmail.com>
2022-01-15 17:34:24 -0500

    Initial Commit

---
+++ SVG_Shape_Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SVG_Shape_Login
+//
+//  Created by Stanley Pan on 2022/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SVG_Shape_Login/SVG_Shape_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVG_Shape_LoginApp.swift
+//  SVG_Shape_Login
+//
+//  Created by Stanley Pan on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SVG_Shape_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
